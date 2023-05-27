eb1597e48cf1fefa8f2ec1aacccb1a71a5fb191c
Himanshu-learner <himanshuchaurasiya1818@gmail.com>
2023-05-27 21:58:41 +0530

    interest calculator

--- program.java
+++ program.java
@@ -5,41 +5,46 @@ class calculation{
        
     int n;
     double principal,time,rate,sinterest,hinterest,binterest;   
-    System.out.println("ENTER YOUR PRINCIPAL TIME AND RATE ");
+    System.out.println("\nENTER YOUR PRINCIPAL TIME AND RATE");
 
     Scanner sc = new Scanner(System.in);
         principal = sc.nextDouble();
         time = sc.nextDouble();
         rate = sc.nextDouble();
 
-        System.out.println("ENTER YOUR PRINCIPAL ="+principal +"TIME="+time +" AND RATE=%"+rate);
+        // Principal , time and rate entered through user
+        System.out.println("\nENTER YOUR PRINCIPAL = "+principal +" TIME = "+time +" AND RATE = "+rate+"%");
 
-        System.out.println("Which bank interest calculator you want?");
-        System.out.print("1.SBI \n2.HDFC \n3.BOB\n");
+        // Preferred bank which mentioned
+        System.out.println("\nWhich bank interest calculator you want?");
+        System.out.print("\n1.SBI \n2.HDFC \n3.BOB\n");
             
         n = sc.nextInt();
 
+        //SBI
        if (n==1) {
         sinterest=(principal*rate*time)/100;
-        System.out.println("SBI INTEREST : "+ sinterest);
-        System.out.println("Total = Principal + Interest \n"+(principal+sinterest));
+        System.out.println("\nSBI INTEREST : "+ sinterest);
+        System.out.println("\nTotal = Principal + Interest \nTotal = "+(principal+sinterest));
        }
 
+       //HDFC
        else if (n==2) {
         hinterest=(principal*rate*time)/100;
-        System.out.println("HDFC INTEREST : "+ hinterest);
-        System.out.println("Total = Principal + Interest \n"+(principal+hinterest));
+        System.out.println("\nHDFC INTEREST : "+ hinterest);
+        System.out.println("\nTotal = Principal + Interest \nTotal = "+(principal+hinterest));
        }
-
+       
+       //BOB
        else if (n==3) {
         binterest=(principal*rate*time)/100;
-        System.out.println("BOB INTEREST : "+ binterest);
-        System.out.println("Total = Principal + Interest \n"+(principal+binterest));
+        System.out.println("\nBOB INTEREST : "+ binterest);
+        System.out.println("\nTotal = Principal + Interest \nTotal = "+(principal+binterest));
        }
 
+       //WRONG INPUT
        else{
-        System.out.println("Please! Choose only mentioned number");
-       }
-      sc.close();  
+        System.out.println("\nPlease! Choose only mentioned number");
+       }  
 }
 }
